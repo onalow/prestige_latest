001109a44c891ab9eecbe7aa16e367c099bb44fe
F F <talktojimmykimmel@gmail.com>
2020-03-13 12:16:57 +0100

    repairs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -37,7 +37,7 @@ class Kernel extends ConsoleKernel
 
         $schedule->command('daily:payout')
                
-                 ->dailyAt('10:35');
+                 ->dailyAt('11:18');
 
       
     }
